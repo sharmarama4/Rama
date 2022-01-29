e7b4acf8c6206293a9b861bba19ee022f8c1e659
sharm <sharm@192.168.0.245>
2022-01-29 22:06:27 +0100

    loops in java

---
+++ eclipscvekurrrtrskkkk/loops/src/loops/Loops.java
@@ -0,0 +1,57 @@
+package loops;
+
+
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+	//for(int i=0; i<100; i++)	{
+		//System.out.println("Even numbers" +i);
+	//}
+		
+		
+		
+		
+		//while(true) {
+		//System.out.println(a);	
+		//a++	;
+		
+		//if(a>100) {
+			//break;
+		
+		//int a = 23;
+		//while(a<=100) {
+		//	System.out.println(a);
+		//a++;
+	//	}
+		
+		
+	//	
+		
+		
+		int a=23;
+		do {
+			System.out.println(a);
+			a++;
+		}while(a>100);
+			
+		}
+		
+		
+		
+		}
+			
+		
+	
+	
+		
+		
+		
+		
+		
+		
+	
+
+
