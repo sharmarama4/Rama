152f1e229fe4f690107cbe8776a0e05f16781793
sharm <sharm@192.168.0.245>
2022-01-29 18:56:45 +0100

    first project

---
+++ eclipscvekurrrtrskkk/ifstatements/src/ifstatements/Ifstatements.java
@@ -0,0 +1,33 @@
+package ifstatements;
+
+public class Ifstatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+		char grade='D';
+		
+		
+		if(grade=='A') {
+			System.out.println("Your grade is very good");
+		}else if(grade=='B') {
+			
+		System.out.println(" Very good,keep learning");	
+			
+			
+		} else if(grade=='C') {
+			System.out.println(" keep improving");
+		}else {
+			System.out.println("Invalid");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
