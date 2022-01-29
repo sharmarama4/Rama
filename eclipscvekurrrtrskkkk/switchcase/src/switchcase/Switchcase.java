f7a00d8fbc9311df80458f52a2a5da02317aad0a
sharm <sharm@192.168.0.245>
2022-01-29 20:58:22 +0100

    logical operator

---
+++ eclipscvekurrrtrskkkk/switchcase/src/switchcase/Switchcase.java
@@ -0,0 +1,25 @@
+package switchcase;
+
+public class Switchcase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		//logical operator &&,||,!
+		int age=12;
+		if(!(age>18 )) {
+		
+		System.out.println("You can vote");
+		}else {
+		
+		System.out.println("You can't vote");
+		}
+		
+		
+		}
+		
+		}	
+		
+	
+
+
