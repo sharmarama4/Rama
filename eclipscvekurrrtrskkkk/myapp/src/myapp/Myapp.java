1e071f79e848a6ad2cc89d3f350f2184ee95b171
sharm <sharm@192.168.0.245>
2022-02-01 20:39:24 +0100

    mytest1

--- eclipscvekurrrtrskkkk/myapp/src/myapp/Myapp.java
+++ eclipscvekurrrtrskkkk/myapp/src/myapp/Myapp.java
@@ -7,25 +7,25 @@ public class Myapp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int dags;
+		 Scanner keyboard= new Scanner(System.in);
+		 System.out.println( "Fill the start day :");
+		  dags=keyboard.nextInt();
+		  System.out.println(" Start from monday to  "+ dags+"are:\n"); 
+		  for(i=1;<=dags;a++) {
+			if(a%2==0)    
+			{
+			System.out.println(a+"\n");	
+			}
+			
+		  }
+			System.out.println("Odd numbers from 1 to"+number+"are:\n");  
+			for(a=1;a<=number;a++) {
+			if(a%2==1) {	
+				System.out.println(a+"\n");
+				
 		
 		
-		
-		String dag;
-		int totaalDags;
-	Scanner r= new Scanner(System.in);	
-		String []dags= {"monday","Tuesday","wednesday","Thursday","Friday"};
-		
-	
-	System.out.println("Fill the start dag:");
-		 dag = r.nextLine();
-for(int i=1; i<dags.length;i++) {
-	System.out.println(dags[i]);}
-		  
-System.out.println("Fill the stop dag");
-        dag=r.nextLine();
-for(int i=1; i<dags.length;i++){
-	System.out.println(dags[i]);}
-	
 	}
 }
  
