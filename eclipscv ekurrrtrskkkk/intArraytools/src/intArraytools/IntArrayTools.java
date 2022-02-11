8cef2b773a4397a3050f26ce13c0011edab60c03
sharm <sharm@192.168.0.100>
2022-02-11 19:19:59 +0100

    commit

---
+++ eclipscv ekurrrtrskkkk/intArraytools/src/intArraytools/IntArrayTools.java
@@ -0,0 +1,22 @@
+package intArraytools;
+
+public class IntArrayTools {
+
+	public static void arrayApp(int[]marks) {
+	for(int i=0;i<marks.length;i++)	{
+		System.out.println(marks[i]);
+	}
+	}
+	
+	public static void main(String[] args) {
+	
+	arrayApp(new int[] {10,20,30,40,50});	
+		
+	
+			
+		}
+		
+		
+	}
+   
+
